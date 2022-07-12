fb53f9ddb0abf1a7ef0799e3795b1eb31854e815
Vilius Luneckas <vilius.luneckas@gmail.com>
2022-07-12 12:56:01 +0300

    Initial commit

---
+++ lib/closed_loop.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "closed_loop/version"
+
+module ClosedLoop
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/closed_loop_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ClosedLoop do
+  it "has a version number" do
+    expect(ClosedLoop::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
